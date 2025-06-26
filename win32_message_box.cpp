b7ebf9c72163e8839e6315122825239411c4f8fb
gbjnewman <gbjnewman@protonmail.com>
2025-06-26 13:46:03 +1000

    added comments to messagebox example

--- win32_message_box.cpp
+++ win32_message_box.cpp
@@ -1,3 +1,7 @@
+/* 
+ * https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-messagebox
+ * needs user32.lib for compilation, ie: cl win32_message_box.cpp user32.lib
+ */
 #include <windows.h>
 
 int CALLBACK
