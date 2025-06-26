17e0c8c122761fb94227dc88da0ba0c650f5e56b
gbjnewman <gbjnewman@protonmail.com>
2025-06-26 13:42:33 +1000

    messagebox example

---
+++ win32_message_box.cpp
@@ -0,0 +1,13 @@
+#include <windows.h>
+
+int CALLBACK
+WinMain(HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nCmdShow)
+{
+	MessageBox(0, "This is the Message", "This is the title",
+		   MB_OK | MB_ICONINFORMATION);
+
+	return(0);
+}
