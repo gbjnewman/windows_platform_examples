2a1f38c4c621e5f4c3a82064b40e90ed1a8f216d
gbjnewman <gbjnewman@protonmail.com>
2025-06-27 20:06:14 +1000

    added more

---
+++ win32_basic_window_creation.cpp
@@ -0,0 +1,120 @@
+#include <windows.h>
+
+LRESULT CALLBACK
+Win32MainWindowCallback(HWND Window,
+						UINT Message,
+						WPARAM WParam,
+						LPARAM LParam)
+{
+	LRESULT Result = 0;
+
+	switch(Message)
+	{
+
+		case WM_SIZE:
+		{
+		} break;
+
+		case WM_CLOSE:
+		{
+		} break;
+
+		case WM_ACTIVATEAPP:
+		{
+		} break;
+
+		case WM_DESTROY:
+		{
+		} break;
+
+		case WM_PAINT:
+		{
+		} break;
+
+		default :
+		{
+			Result = DefWindowProc(	Window,
+									Message,
+									WParam,
+									LParam);
+		} break;
+	}
+
+	return(Result);
+
+}
+
+// windows entry point(the main function for windows graphical programs)
+int CALLBACK
+WinMain(HINSTANCE hInstance,
+		HINSTANCE hPrevInstance,
+		LPSTR lpCmdLine,
+		int nCmdShow)
+{
+	// casey's explaination of how to open a window in windows
+	// https://youtu.be/4ROiWonnWGk?si=voZuPeSIwZlMzviy&t=179
+
+	// WNDCLASS is a struct, here it's members are being initialised
+	// with all zeros https://youtu.be/4ROiWonnWGk?si=jPsLze0-AGLYj0rQ&t=579
+	// https://learn.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-wndclassa
+	WNDCLASS WindowClass = {};
+
+	//WindowClass.style;
+	WindowClass.lpfnWndProc = Win32MainWindowCallback;
+	//WindowClass.cbClsExtra;
+	//WindowClass.cbWndExtra;
+	WindowClass.hInstance = hInstance;
+	//WindowClass.hIcon;
+	//WindowClass.hCursor;
+	//WindowClass.hbrBackground;
+	//WindowClass.lpszMenuName;
+	WindowClass.lpszClassName = "HandmadeHeroWindowClass";
+
+	if(RegisterClass(&WindowClass))
+	{
+		//casey create window explanation
+		//https://youtu.be/4ROiWonnWGk?si=kHx4s38HZ_ZHbceN&t=2334
+		HWND WindowHandle = CreateWindowExA(
+			0,									//DWORD dwExStyle
+			WindowClass.lpszClassName,			//LPCTSTR lpClassName
+			"Handmade Hero",					//LPCTSTR lpWindowName
+			WS_OVERLAPPEDWINDOW|WS_VISIBLE,		//DWORD dwStyle
+			CW_USEDEFAULT,						//int x
+			CW_USEDEFAULT,						//int y
+			CW_USEDEFAULT,						//int nWidth
+			CW_USEDEFAULT,						//int nHeight
+			0,									//HWND hWndParent
+			0,									//HMENU hMenu
+			hInstance,							//HINSTANCE hInstance
+			0);									//LPVOID lpParam
+		if(WindowHandle)
+		{
+			Running = true;
+			while(Running)
+			{
+				MSG Message;
+				BOOL MessageResult = GetMessageA(&Message, 0, 0, 0);
+				if(MessageResult > 0)
+				{
+					TranslateMessage(&Message);
+					DispatchMessageA(&Message);
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+		else
+		{
+			//TODO: logging
+		}
+	}
+	else
+	{
+		//TODO: logging
+	}
+
+	return(0);
+
+}
