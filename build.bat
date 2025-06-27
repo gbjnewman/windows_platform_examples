f243d3b27c4d37a7aba27529c232e140729877b6
gbjnewman <gbjnewman@protonmail.com>
2025-06-27 21:01:12 +1000

    build bat added

--- build.bat
+++ build.bat
@@ -2,11 +2,15 @@
 @echo off 
 
 :: make directory
-mkdir executables
+if not exist \executables (
+    mkdir \executables
+)
 
 :: push directory
 pushd  executables
+
+dir
 :: the -Zi switch make it so debug info is generated as well https://youtu.be/Ee3EtYb8d1o?si=lO9-bJeU9AM2UzRI&t=2531
-cl -Zi ..\source_code\win32_handmade.cpp user32.lib Gdi32.lib
+cl ..\%1 user32.lib Gdi32.lib
 :: pop directory
 popd
